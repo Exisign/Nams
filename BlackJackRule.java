0f16090577fdac880a5c26dcef11ab93b0a75c2d
exisign <exisign777@gmail.com>
2018-09-18 01:39:53 +0900

    º�Àè ³»¿ë¼öÁ¤

--- BlackJackRule.java
+++ BlackJackRule.java
@@ -5,10 +5,16 @@ abstract class BlackJackRule extends BlackJackDeck{
 	abstract void stand();
 	abstract void bust();
 	
+	boolean b_stay;	//stay 여부 판
+	
 	
 	//hit card 
 	public Card hit(){
 		return super.popCard();
+	}
+	
+	public void CheckBust(BlackJackUser c){
+	
 	}
 	
 }
--- BlackJackUser.java
+++ BlackJackUser.java
@@ -1,13 +1,42 @@
 import java.util.LinkedList;
 
 public class BlackJackUser {
+	private int money;
 	private LinkedList hand = new LinkedList();
 	
 	BlackJackUser(){
-		System.out.println("들어온 패가 없습니다.");
+		money = 0;
+		System.out.println("유저 참가");
 	}
-	BlackJackUser(Card c){
+	
+	void addCard(Card c){
 		hand.add(c);
-		System.out.println("손에 카드가 " + hand.size() + "장 들어왔습니다.");
+		System.out.println("손에 카드가 " + hand.size() + "장 있습니다.");
 	}
+	
+	int count(){
+		
+		int count = 0;
+		
+		for(int i = 0; i<hand.size(); i++){
+			if(hand.get(i) instanceof Card){
+				count = ((Card)hand.get(i)).showNum();
+			}else{
+				System.out.println("다형성 문제");
+			}
+		}
+		
+		return count;
+	}
+	
+	int betMoney(int money){
+		if(this.money-money>0){
+			this.money -= money;
+			return money;
+		}
+		
+		return -1;
+	}
+	
+	void getMoney(int money){	this.money += money;	}
 }
